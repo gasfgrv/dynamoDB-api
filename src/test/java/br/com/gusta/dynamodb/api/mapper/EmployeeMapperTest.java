077c83496a30e97b7e74800cadc9c56817057879
Gustavo Silva <gustavo_almeida11@hotmail.com>
2022-12-31 00:05:27 -0300

    Testes unitários do EmployeeMapper

--- src/test/java/br/com/gusta/dynamodb/api/mapper/EmployeeMapperTest.java
+++ src/test/java/br/com/gusta/dynamodb/api/mapper/EmployeeMapperTest.java
@@ -3,59 +3,59 @@ package br.com.gusta.dynamodb.api.mapper;
 import br.com.gusta.dynamodb.api.model.EmployeeDto;
 import br.com.gusta.dynamodb.api.model.EmployeeInput;
 import br.com.gusta.dynamodb.domain.model.Employee;
-import br.com.gusta.dynamodb.mocks.EmployeeMock;
-import static org.assertj.core.api.Assertions.assertThat;
+import br.com.gusta.dynamodb.mocks.MockUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import static org.mockito.Mockito.doReturn;
 import org.modelmapper.ModelMapper;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@Profile("test")
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = {EmployeeMapper.class, ModelMapper.class})
 class EmployeeMapperTest {
-
-    public static Employee EMPLOYEE;
-
-    public static EmployeeDto EMPLOYEE_DTO;
-
-    public static EmployeeInput EMPLOYEE_INPUT;
-
-    @Mock
-    private ModelMapper modelMapper;
 
     @InjectMocks
     private EmployeeMapper mapper;
 
+    @Mock
+    private ModelMapper modelMapper;
+
+    public Employee employee;
+
+    public EmployeeDto employeeDto;
+
+    public EmployeeInput employeeInput;
+
     @BeforeEach
     void setUp() {
-        EMPLOYEE = EmployeeMock.get();
-        EMPLOYEE_DTO = EmployeeMock.getDto();
-        EMPLOYEE_INPUT = EmployeeMock.getInput();
+        employee = MockUtils.getEmployeeEntity();
+        employeeDto = MockUtils.getEmployeeDto();
+        employeeInput = MockUtils.getEmployeeInput();
     }
 
     @Test
     void toEntityTest() {
-        doReturn(EMPLOYEE)
-                .when(modelMapper)
-                .map(EMPLOYEE_INPUT, EMPLOYEE.getClass());
+        given(modelMapper.map(employeeInput, Employee.class)).willReturn(employee);
 
-        assertThat(mapper.toEntity(EMPLOYEE_INPUT))
-                .isInstanceOf(Employee.class);
+        assertThat(mapper.toEntity(employeeInput)).isInstanceOf(Employee.class);
+
+        verify(modelMapper, times(1)).map(employeeInput, Employee.class);
     }
 
     @Test
     void toDtoTest() {
-        doReturn(EMPLOYEE_DTO)
-                .when(modelMapper)
-                .map(EMPLOYEE, EMPLOYEE_DTO.getClass());
+        given(modelMapper.map(employee, EmployeeDto.class)).willReturn(employeeDto);
 
-        assertThat(mapper.toDto(EMPLOYEE))
-                .isInstanceOf(EmployeeDto.class);
+        assertThat(mapper.toDto(employee)).isInstanceOf(EmployeeDto.class);
+
+        verify(modelMapper, times(1)).map(employee, EmployeeDto.class);
     }
 }

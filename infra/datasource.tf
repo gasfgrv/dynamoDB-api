03bec14fdc6d03f1e6abd64d4b2d52397106f27c
Gustavo Silva <gustavo_almeida11@hotmail.com>
2023-02-21 23:35:26 -0300

    remoção da infra terraform

--- infra/datasource.tf
+++
@@ -1,9 +0,0 @@
-data "aws_ami" "api_ami" {
-  most_recent = true
-  owners = ["099720109477"]
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20230208"]
-  }
-}
